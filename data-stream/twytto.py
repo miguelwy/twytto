d5360743e2c4bbae8a47154476c294ca622b727f
Miguel Wychovaniec <mwychovaniec@segware.com>
2021-11-09 15:47:49 -0300

    Initial commit

---
+++ data-stream/twytto.py
@@ -0,0 +1,27 @@
+import tweepy
+from keys import *
+from kafka import KafkaProducer
+
+class MaxStream(tweepy.Stream):
+
+    def on_data(self,raw_data):
+        self.process_data(raw_data)
+        return True
+
+    def process_data(self, raw_data):
+        producer.send("crypto", raw_data)
+        print(raw_data)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+    def start(self):
+        self.stream.filter(track=["crypto"])
+
+if __name__ == '__main__':
+    
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    stream = MaxStream( API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    stream.sample()
